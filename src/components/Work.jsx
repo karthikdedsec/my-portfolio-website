55aa5e35df3564c1ae03546fa06dd950426374d3
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-02-19 15:02:19 +0530

    add a new project section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,6 +10,7 @@ import realtor from "../assets/projects/realtor.png";
 import nike from "../assets/projects/nike.png";
 import lightnin from "../assets/projects/lightnin.png";
 import netflix from "../assets/projects/netflix.png";
+import blog from "../assets/projects/blog.png";
 
 export default function Work() {
   return (
@@ -27,6 +28,32 @@ export default function Work() {
         {/* container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
           {/* Grid item */}
+          <div
+            style={{
+              backgroundImage: `url(${blog})`,
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* hover effect */}
+            <div className="mx-4 opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold font-realtor text-white tracking-wider">
+                Blog site created using Reactjs tailwind Nodejs express and
+                MongoDB
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://karthik-blog-ohyt.onrender.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold font-realtor text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/karthikdedsec/karthik-s-blog-mern">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold font-realtor text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{
               backgroundImage: `url(${lightnin})`,
